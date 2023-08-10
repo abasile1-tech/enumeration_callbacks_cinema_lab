3f71ef1cee6062635942107a3107a6c6927b58ea
John <ashton.basile@gmail.com>
2023-08-10 14:22:42 +0100

    I took the starter code provided by CodeClan (which was over eight years old) and updated it to include classes and more up-to-date formatting

---
+++ specs/film_spec.js
@@ -0,0 +1,32 @@
+import { strictEqual } from 'assert';
+import Film from '../models/film.js';
+
+describe('Film', function () {
+
+  let moonlight;
+
+  beforeEach(function () {
+    moonlight = new Film('Moonlight', 'drama', 2016, 111);
+  });
+
+  it('should have a title', function () {
+    const actual = moonlight.title;
+    strictEqual(actual, 'Moonlight');
+  });
+
+  it('should have a genre', function () {
+    const actual = moonlight.genre;
+    strictEqual(actual, 'drama');
+  });
+
+  it('should have a year', function () {
+    const actual = moonlight.year;
+    strictEqual(actual, 2016);
+  });
+
+  it('should have a length', function () {
+    const actual = moonlight.length;
+    strictEqual(actual, 111);
+  });
+
+});
