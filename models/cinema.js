79a0efeb00e9775cac4f4004b94f96b944fac5e8
John <ashton.basile@gmail.com>
2023-08-10 15:45:10 +0100

    I wrote the methods for the Cinema class and all tests are passing

--- models/cinema.js
+++ models/cinema.js
@@ -2,6 +2,42 @@ class Cinema {
 	constructor(films) {
 		this.films = films;
 	}
+
+	getTitles() {
+		return this.films.map((film) => {
+			return film.title;
+		});
+	}
+
+	findByTitle(title) {
+		return this.films.find((film) => {
+			return film.title === title;
+		});
+	}
+
+	filterByGenre(genre) {
+		return this.films.filter((film) => {
+			return film.genre === genre;
+		});
+	}
+
+	checkYear(year) {
+		return this.films.some((film) => {
+			return film.year === year;
+		});
+	}
+
+	checkLengthGreaterThan(length) {
+		return this.films.every((film) => {
+			return film.length >= length;
+		});
+	}
+
+	calculateTotalRunningTime() {
+		return this.films.reduce((total, film) => {
+			return total += film.length;
+		}, 0);
+	}
 }
 
 module.exports = Cinema;
